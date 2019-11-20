cbeb9d976722c278742735f4eed2a1d74c422e7a
colorlessenergy <brianmunozdev@gmail.com>
2019-11-20 09:18:25 -0800

    add a array to the user schema to store all post id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const Router = express.Router();
 const usersController = require('../controllers/user');
+const authController = require('../controllers/auth');
 
 Router.route('/')
+  .get(authController.validateToken, usersController.getUserPosts)
   .post(usersController.registerUser);
 
 

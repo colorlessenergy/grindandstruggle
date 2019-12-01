5b3aa2b5da680c4ef3703598bf0ff278af3b550f
colorlessenergy <brianmunozdev@gmail.com>
2019-12-01 10:28:28 -0800

    delete JWT from DB and from localstorage

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
 
-const navbar = props => {
-  // nav items if user is not logged in
-  let navItems = (
+import { connect } from 'react-redux';
+import { logoutAction } from '../../store/actions/authAction'
+
+class NavBar extends Component {
+  render() {
+    // nav items if user is not logged in
+    let NavItems = (
       <nav>
         <ul>
           <li><NavLink to="/login">Login</NavLink></li>
@@ -12,20 +16,36 @@ const navbar = props => {
         </ul>
       </nav>
     );
-  // nav items if user is logged in
-  if (localStorage.token) {
-    navItems = (
-      <nav>
-        <ul>
-          <li><NavLink to="/create">Create Post</NavLink></li>
-          <li><NavLink to="/">Grind and Struggle</NavLink></li>
-          <li><NavLink to="/logout">Logout</NavLink></li>
-        </ul>
-      </nav>
-    );
+    // nav items if user is logged in
+    if (localStorage.token) {
+      NavItems = (
+        <nav>
+          <ul>
+            <li><NavLink to="/create">Create Post</NavLink></li>
+            <li><NavLink to="/">Grind and Struggle</NavLink></li>
+            <li onClick={this.logoutUser}>Logout</li>
+          </ul>
+        </nav>
+      );
+    }
+
+    return NavItems;
   }
 
-  return navItems;
+  /**
+   * removes the JWT from the database and from localstorage
+   */
+  logoutUser = () => {
+    this.props.logout();
+  }
 };
 
-export default navbar;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout() {
+      return dispatch(logoutAction());
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(NavBar);
